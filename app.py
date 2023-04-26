460d1fa63af0362c71b71608049de13485be4a50
MudiW <mudiwaked@gmail.com>
2023-04-26 13:05:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -68,17 +68,24 @@ def delete_item(id):
     db.delete_item(id)
     return jsonify({ 'success': True })
 
-def send_request(target_ip, start_num, stop_num, color):
+def send_request2(target_ip, start_num, stop_num, color):
     url = f"http://{target_ip}/json/state" # construct URL using the target IP address
     state = {"seg": [{"id": 0, "start": start_num, "stop": stop_num, "col": [color]}]}
     response = requests.post(url, json=state)
 
+def send_request(target_ip, start_num, stop_num, color):
+    url = f"http://{target_ip}/json/state" # construct URL using the target IP address
+    state = {"seg": [{"id": 1, "start": start_num, "stop": stop_num, "col": [color]}]}
+    response = requests.post(url, json=state)
+
+
 def lights(position, pi):
     start_num = int(position) - 1
-    send_request(pi, start_num, int(position), [255, 255, 255]) # Convert color value to [0, 0, 0, 255] to only use white part of LED (RGBW LEDs only).
-    time.sleep(5) # Change how long the LED stays on for.
-    send_request(pi, 0, 60, [0, 255, 0])
-
+    send_request2(pi, 0, 64, [255, 0, 0])
+    send_request(pi, start_num, int(position), [0, 255, 0])
+    time.sleep(2) # Change how long the LED stays on for.
+    send_request2(pi, 0, 64, [255, 255, 255])
+    send_request(pi, 0, 64, [255, 255, 255])
 # Running the Flask application
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
